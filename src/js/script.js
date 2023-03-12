47db2f17ef7b2e4bae8abbe2fe9aa67595acaed3
Erkin <erkinred@gmail.com>
2023-03-13 01:44:54 +0600

    add validation script

--- src/js/script.js
+++ src/js/script.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
 				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
 			});
 		});
-	}
+	};
 
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
@@ -58,4 +58,36 @@ $(document).ready(function(){
 			$('.overlay, #order').fadeIn('slow');
 		})
 	});
+
+	//Valide forms
+	function valideForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+				  required: true,
+				  email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите {0} символа!")
+				},
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+					required: "Пожалуйста, введите свою номер почту",
+					email: "Неправильно введен адрес почты"
+				}
+			}
+		});
+	};
+
+	valideForms('#consultation-form');
+	valideForms('#consultation form');
+	valideForms('#order form');
 }); 
